226fec43bb9da6a7a746b48fbd4c957d2290d3e5
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2024-02-14 13:15:40 +0400

    jjjj

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-let todoListBlock = document.querySelector(".todolist__block");
-let texts = document.querySelector(".text");
-let input = document.querySelector(".todolist__input");
-let textElement = document.querySelector(".text");
+const tasks = [];
+const input = document.querySelector(".todolist__input");
+const texts = document.querySelector(".text");
+const todolistButton = document.querySelector(".todolist__button")
+
 const listInput = () => {
-  let inputValue = input.value;
+  const inputValue = input.value;
   if (inputValue !== "") {
-    let newText = `
-      <li>
-        <p class="text__page">${inputValue}<span class="span">&#10007</span></p>
-      </li>`;
-    texts.innerHTML += newText;
+    const newTask = {
+      text: inputValue,
+      completed: false
+    };
+    tasks.push(newTask);
+    renderTasks();
     input.value = "";
   }
 };
+
 const del = (event) => {
-    let text = event.target.closest(".text__page");
-    //event.target.closest(".text__page") - это метод JavaScript, который ищет ближайший родительский элемент (вверх по иерархии) у элемента, 
-    //на котором произошло событие event, и который соответствует селектору ".text__page". 
-    //Этот метод полезен, когда вам нужно найти определенный элемент внутри иерархии DOM.
+  const text = event.target.closest(".text__page");
+  if (text) {
+    const taskIndex = text.parentElement.dataset.index;
+    tasks.splice(taskIndex, 1);
+    renderTasks();
+  }
+};
 
-    if (text) {
-      text.parentElement.remove();
-      //text.parentElement.remove(); - 
-      // это JavaScript код,
-      //который удаляет родительский элемент элемента text из DOM. 
-      //То есть, удаляется элемент, который содержит text.
-    }
-  };
- //event используется для обращения к объекту события и получения информации о нем, 
- //например, элементе, 
-//на котором произошло событие, и выполнения соответствующих действий.
+const renderTasks = () => {
+  let taskList = "";
+  tasks.forEach((task, index) => {
+    const completedClass = task.completed ? "completed" : "";
+    const taskElement = `
+      <li data-index="${index}">
+        <p class="text__page ${completedClass}">${task.text}<span class="span">&#10007</span></p>
+      </li>`;
+    taskList += taskElement;
+  });
+  texts.innerHTML = taskList;
+};
 
-//event.target в JavaScript представляет собой свойство объекта event, 
-//которое содержит ссылку на элемент, на котором произошло событие. 
+todolistButton.addEventListener("click", listInput);
+texts.addEventListener("click", del);
 
-//обработчики событий
-textElement.addEventListener("click", del);
-todoListBlock.addEventListener("click", listInput);
+renderTasks();
